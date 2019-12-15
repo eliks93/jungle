32779558df0ef0ea9bc975cf9d76d7a659c546d9
Eli Kittross-Schnell <eliks92@gmail.com>
2019-12-15 02:36:36 +0000

    added registration, login, lougout functionality with bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root to: 'products#index'
-
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   get 'about', to: 'about#show'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   resource :cart, only: [:show] do
     post   :add_item
